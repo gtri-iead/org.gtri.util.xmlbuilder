45295566acbcfce3d401885641ec2cac4f5b1e4d
Lance Gatlin <lance.gatlin@gtri.gatech.edu>
2012-11-14 06:17:11 -0500

    Fixes to XmlFactory

--- main/src/main/java/org/gtri/util/xmlbuilder/XmlFactory.java
+++ main/src/main/java/org/gtri/util/xmlbuilder/XmlFactory.java
@@ -22,16 +22,24 @@
 
 package org.gtri.util.xmlbuilder;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
-import org.apache.xml.serialize.OutputFormat;
-import org.apache.xml.serialize.XMLSerializer;
-import org.gtri.util.iteratee.impl.Consumer;
-import org.gtri.util.iteratee.impl.Producer;
-import org.gtri.util.xmlbuilder.impl.XmlEvent;
+import javax.xml.stream.XMLStreamWriter;
+import net.sf.saxon.s9api.Processor;
+import net.sf.saxon.s9api.SaxonApiException;
+import net.sf.saxon.s9api.Serializer;
+import org.gtri.util.iteratee.api.Consumer;
+import org.gtri.util.iteratee.api.Lazy;
+import org.gtri.util.iteratee.api.Producer;
+import org.gtri.util.xmlbuilder.api.XmlEvent;
 import org.gtri.util.xmlbuilder.impl.XmlReader;
 import org.gtri.util.xmlbuilder.impl.XmlWriter;
         
@@ -39,37 +47,79 @@ import org.gtri.util.xmlbuilder.impl.XmlWriter;
  *
  * @author Lance
  */
-public class XmlFactory {
+public final class XmlFactory implements org.gtri.util.xmlbuilder.api.XmlFactory {
   private XmlFactory() { }
   public static final XmlFactory INSTANCE = new XmlFactory();
   public static XmlFactory instance() { return INSTANCE; }
   public static final int STD_CHUNK_SIZE = 256;
-  
-  public Producer<XmlEvent> createXmlReader(XMLStreamReader xmlStreamReader, int chunkSize) {
-    return new XmlReader(xmlStreamReader, chunkSize);
+
+  @Override
+  public Producer<XmlEvent> createXmlReader(final XMLStreamReaderFactory factory, int chunkSize) {
+    return new XmlReader(factory, chunkSize);
   }
   
-  public Producer<XmlEvent> createXmlReader(XMLStreamReader xmlStreamReader) {
-    return createXmlReader(xmlStreamReader, STD_CHUNK_SIZE);
+  public Producer<XmlEvent> createXmlReader(final XMLStreamReaderFactory factory) {
+    return createXmlReader(factory, STD_CHUNK_SIZE);
   }
   
-  public Producer<XmlEvent> createXmlReader(InputStream in) throws XMLStreamException {
-    XMLStreamReader reader = XMLInputFactory.newInstance().createXMLStreamReader(in);
-    return createXmlReader(reader, STD_CHUNK_SIZE);
+  public Producer<XmlEvent> createXmlReader(final InputStream in) {
+    final Lazy<ByteArrayOutputStream> lazyBaos = new Lazy<ByteArrayOutputStream>() {
+      @Override
+      public ByteArrayOutputStream init() {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        byte[] buf = new byte[1024];
+        int n;
+        try {
+          n = in.read(buf);
+          while (n > 0) {
+            baos.write(buf, 0, n);
+            n = in.read(buf);
+          }
+        } catch (IOException ex) {
+          Logger.getLogger(XmlFactory.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return baos;
+      }
+    };
+    
+    return createXmlReader(
+      new XMLStreamReaderFactory() {
+        @Override
+        public XMLStreamReader create() throws XMLStreamException {
+          byte[] content = lazyBaos.get().toByteArray();
+          return XMLInputFactory.newInstance().createXMLStreamReader(new ByteArrayInputStream(content));
+        }
+      }, 
+      STD_CHUNK_SIZE
+    );
+  }
+
+  @Override
+  public Consumer<XmlEvent> createXmlWriter(final XMLStreamWriterFactory factory) {
+    return new XmlWriter(factory);
   }
   
-  public Consumer<XmlEvent> createXmlWriter(XMLSerializer serializer) {
-    return new XmlWriter(serializer);
-  }
-  
-  public Consumer<XmlEvent> createXmlWriter(OutputStream out, OutputFormat outputFormat) {
-    return createXmlWriter(new XMLSerializer(out, outputFormat));
-  }
-  
-  public Consumer<XmlEvent> createXmlWriter(OutputStream out) {
-    OutputFormat outputFormat = new OutputFormat("XML","UTF-8",true);
-    outputFormat.setIndent(2);
-    outputFormat.setIndenting(true);
-    return createXmlWriter(new XMLSerializer(out, outputFormat));
+  public Consumer<XmlEvent> createXmlWriter(final OutputStream out) {
+    return createXmlWriter(new XMLStreamWriterFactory(){ 
+
+      @Override
+      public XMLStreamWriter create() throws XMLStreamException {
+        Processor p = new Processor(false);
+        Serializer s = new Serializer(out);
+        s.setProcessor(p);
+        s.setOutputProperty(Serializer.Property.ENCODING, "UTF-8");
+        s.setOutputProperty(Serializer.Property.INDENT, "yes");
+        s.setOutputProperty(Serializer.Property.SAXON_INDENT_SPACES, "2");
+        s.setOutputProperty(Serializer.Property.SAXON_LINE_LENGTH, "80");
+        try {
+            return s.getXMLStreamWriter();
+        } catch (SaxonApiException ex) {
+          throw new RuntimeException(ex);
+        }
+      }
+      //        return XMLOutputFactory.newInstance().createXMLStreamWriter(out, "UTF-8");
+      
+      
+    });
   }
 }
