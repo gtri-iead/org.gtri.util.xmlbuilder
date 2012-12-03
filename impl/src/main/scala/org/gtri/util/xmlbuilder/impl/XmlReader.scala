42cc3ce19752ec24571ac65f08fe5624519b50af
Lance Gatlin <lance.gatlin@gtri.gatech.edu>
2012-12-03 18:37:10 -0500

    Added peek parsing for single-value elements

--- impl/src/main/scala/org/gtri/util/xmlbuilder/impl/XmlReader.scala
+++ impl/src/main/scala/org/gtri/util/xmlbuilder/impl/XmlReader.scala
@@ -22,7 +22,7 @@
 
 package org.gtri.util.xmlbuilder.impl
 
-import collection.mutable.ArrayBuffer
+
 import javax.xml.stream.{XMLStreamReader, XMLStreamConstants}
 import org.gtri.util.xsddatatypes._
 import org.gtri.util.xmlbuilder.api.XmlEvent
@@ -33,6 +33,7 @@ import org.gtri.util.iteratee.impl.{Enumerators, ImmutableBuffers}
 import org.gtri.util.iteratee.impl.Enumerators._
 
 import org.gtri.util.iteratee.impl.ImmutableBuffers.Conversions._
+import annotation.tailrec
 
 /**
  * Created with IntelliJ IDEA.
@@ -41,60 +42,79 @@ import org.gtri.util.iteratee.impl.ImmutableBuffers.Conversions._
  * Time: 6:49 PM
  * To change this template use File | Settings | File Templates.
  */
-object XmlReader {
-}
 class XmlReader(factory : XMLStreamReaderFactory, val chunkSize : Int = 256) extends Enumerator[XmlEvent] {
-  def initialState() = Cont(factory.create())
+  def initialState() = {
+    val result = factory.create()
+    Cont(result.reader(), new Progress(0,0,result.totalByteSize))
+  }
 
-  case class Cont(reader : XMLStreamReader) extends Enumerators.Cont[XmlEvent] {
-    // TODO: provide progress somehow
-    def progress = Progress.empty
+  case class Cont(reader : XMLStreamReader, val progress : Progress) extends Enumerators.Cont[XmlEvent] {
 
     def step() = {
-      val buffer = new ArrayBuffer[XmlEvent](chunkSize)
+      // Note: may exceed buffer size due to peek - this shouldn't matter downstream though
+      val buffer = new collection.mutable.ArrayBuffer[XmlEvent](chunkSize)
       while(buffer.size < chunkSize && reader.hasNext) {
-        for(event <- nextEvent) {
+        for(event <- nextEvents().reverse) {
           buffer.append(event)
         }
       }
+      val nextProgress = {
+        if(progress.totalItemCount > 0) {
+          val charOffset = reader.getLocation.getCharacterOffset
+          // -1 is returned after eoi reached
+          if(charOffset == -1) {
+            new Progress(0,progress.totalItemCount,progress.totalItemCount)
+          } else {
+            new Progress(0,reader.getLocation.getCharacterOffset,progress.totalItemCount)
+          }
+        } else {
+          Progress.empty
+        }
+      }
       if(buffer.nonEmpty) {
-        Result(this, collection.immutable.Vector(buffer: _*), ImmutableBuffers.empty)
+        Result(Cont(reader, nextProgress), collection.immutable.Vector(buffer: _*), ImmutableBuffers.empty)
       } else {
-        Result(Success(progress), ImmutableBuffers.empty, ImmutableBuffers.empty)
+        Result(Success(nextProgress), ImmutableBuffers.empty, ImmutableBuffers.empty)
       }
     }
 
-    private def nextEvent : Option[XmlEvent] = {
-      reader.next match {
-        case XMLStreamConstants.START_DOCUMENT=> {
-          Some(StartXmlDocumentEvent(
-            reader.getEncoding,
-            reader.getVersion,
-            reader.isStandalone,
-            reader.getCharacterEncodingScheme,
-            getLocatorFromReader
-          ))
+    // Gets the next event - if next event is an element will peek at next few elements to try to extract value
+    // and will return the events it peeked at inaddition to the AddXmlElementEvent
+    private def nextEvents() : List[XmlEvent] = {
+        if(reader.hasNext) {
+        reader.next match {
+          case XMLStreamConstants.START_DOCUMENT=> {
+            List(StartXmlDocumentEvent(
+              reader.getEncoding,
+              reader.getVersion,
+              reader.isStandalone,
+              reader.getCharacterEncodingScheme,
+              getLocatorFromReader
+            ))
+          }
+          case XMLStreamConstants.END_DOCUMENT => {
+            List(EndXmlDocumentEvent(getLocatorFromReader))
+          }
+          case XMLStreamConstants.START_ELEMENT => {
+            val (element, peekQueue) = fetchElementFromReader()
+            peekQueue ::: AddXmlElementEvent(element, getLocatorFromReader) :: Nil
+          }
+          case XMLStreamConstants.END_ELEMENT => {
+            List(EndXmlElementEvent(getElementQNameFromReader, getLocatorFromReader))
+          }
+          case XMLStreamConstants.CHARACTERS => {
+            List(AddXmlTextEvent(reader.getText(), getLocatorFromReader))
+          }
+          case XMLStreamConstants.CDATA => {
+            List(AddXmlTextEvent(reader.getText(), getLocatorFromReader))
+          }
+          case XMLStreamConstants.COMMENT => {
+            List(AddXmlCommentEvent(reader.getText(), getLocatorFromReader))
+          }
+          case _ => nextEvents()
         }
-        case XMLStreamConstants.END_DOCUMENT => {
-          Some(EndXmlDocumentEvent(getLocatorFromReader))
-        }
-        case XMLStreamConstants.START_ELEMENT => {
-          Some(AddXmlElementEvent(fetchElementFromReader(), getLocatorFromReader))
-
-        }
-        case XMLStreamConstants.END_ELEMENT => {
-          Some(EndXmlElementEvent(getElementQNameFromReader, getLocatorFromReader))
-        }
-        case XMLStreamConstants.CHARACTERS => {
-          Some(AddXmlTextEvent(reader.getText(), getLocatorFromReader))
-        }
-        case XMLStreamConstants.CDATA => {
-          Some(AddXmlTextEvent(reader.getText(), getLocatorFromReader))
-        }
-        case XMLStreamConstants.COMMENT => {
-          Some(AddXmlCommentEvent(reader.getText(), getLocatorFromReader))
-        }
-        case _ => None
+      } else {
+        Nil
       }
     }
 
@@ -105,9 +125,9 @@ class XmlReader(factory : XMLStreamReaderFactory, val chunkSize : Int = 256) ext
       new XsdQName(prefix, uri, localName)
     }
 
-    private def fetchElementFromReader() : XmlElement = {
-      val qName = getElementQNameFromReader
-
+//    private def fetchElementFromReader(peekQueue : collection.mutable.Queue[XmlEvent]) : XmlElement = {
+    private def fetchElementFromReader() : (XmlElement, List[XmlEvent]) = {
+        val qName = getElementQNameFromReader
       val attributes = {
         for(i <- 0 until reader.getAttributeCount())
         yield {
@@ -129,9 +149,8 @@ class XmlReader(factory : XMLStreamReaderFactory, val chunkSize : Int = 256) ext
         }
       }
 
-      // TODO: peek for value
-      val value = None
-      XmlElement(qName, value, attributes.toMap, prefixes.toMap)
+      val (value, peekQueue) = peekParseElementValue()
+      (XmlElement(qName, value, attributes.toMap, prefixes.toMap), peekQueue)
     }
 
     private def newXsdAnyURI(uri: String) = {
@@ -152,188 +171,33 @@ class XmlReader(factory : XMLStreamReaderFactory, val chunkSize : Int = 256) ext
       val l = reader.getLocation()
       XmlFileLocator(l.getCharacterOffset, l.getColumnNumber, l.getLineNumber, l.getPublicId, l.getSystemId)
     }
+    // Peek at the next few XmlEvents - if it is a string of text events followed by an end event then compress
+    // the sequence by extracting the combined "value" of the text events and throw away the individual
+    // text events
+    @tailrec
+    private def peekParseElementValue(peekQueue : List[XmlEvent] = Nil) : (Option[String], List[XmlEvent]) = {
+      val events = nextEvents()
+      events match {
+        case List(e:AddXmlTextEvent) => {
+          peekParseElementValue(e :: peekQueue)
+        }
+        case List(e:EndXmlElementEvent) => {
+          if(peekQueue.nonEmpty) {
+            val result = peekQueue.foldLeft(new StringBuilder) {
+              (s,event) =>
+                event match {
+                  case AddXmlTextEvent(text,_) => s.append(text)
+                }
+            }
+            (Some(result.toString), events)
+          } else {
+            (None, events)
+          }
+        }
+        case _ => {
+          (None, events ::: peekQueue)
+        }
+      }
+    }
   }
 }
-
-//class XmlReader(factory : XMLStreamReaderFactory, val chunkSize : Int = 256) extends Enumerator[XmlEvent] {
-//
-//  def initialState = XmlReader.State(factory.create())
-//}
-//
-//
-//
-//class XmlReader(factory : XMLStreamReaderFactory, val chunkSize : java.lang.Integer = 8) extends Producer[XmlEvent] {
-//
-//  def enumerator : Enumerator[XmlEvent] = {
-//    new Enumerator[XmlEvent] {
-//
-//      private val outputBuffer = new ArrayBuffer[XmlEvent](chunkSize)
-//      private val inputQueue = new Queue[XmlEvent]()
-//      private val reader = factory.create()
-//
-//      def enumerate[V](i : Iteratee[XmlEvent,V]) : Iteratee[XmlEvent,V] = {
-//        doEnumerate(next, i)
-//      }
-//
-//      def close() {
-//        reader.close()
-//      }
-//
-//      @tailrec
-//      private def next : Option[XmlEvent] = {
-//        if(inputQueue.nonEmpty) {
-//          Some(inputQueue.dequeue)
-//        } else {
-//          if(reader.hasNext) {
-//            reader.next match {
-//              case XMLStreamConstants.START_DOCUMENT=> {
-//                Some(StartXmlDocumentEvent(
-//                  reader.getEncoding,
-//                  reader.getVersion,
-//                  reader.isStandalone,
-//                  reader.getCharacterEncodingScheme,
-//                  getLocatorFromReader
-//                ))
-//              }
-//              case XMLStreamConstants.END_DOCUMENT => {
-//                Some(EndXmlDocumentEvent(getLocatorFromReader))
-//              }
-//              case XMLStreamConstants.START_ELEMENT => {
-////                val value = peekParseElementValue(next, Nil)
-////                Some(AddXmlElementEvent(getElementFromReader(value), getLocatorFromReader))
-//                workAroundTailRecIssue
-//              }
-//              case XMLStreamConstants.END_ELEMENT => {
-//                Some(EndXmlElementEvent(getElementQNameFromReader, getLocatorFromReader))
-//              }
-//              case XMLStreamConstants.CHARACTERS => {
-//                Some(AddXmlTextEvent(reader.getText(), getLocatorFromReader))
-//              }
-//              case XMLStreamConstants.CDATA => {
-//                Some(AddXmlTextEvent(reader.getText(), getLocatorFromReader))
-//              }
-//              case XMLStreamConstants.COMMENT => {
-//                Some(AddXmlCommentEvent(reader.getText(), getLocatorFromReader))
-//            }
-//              case _ =>
-//                next
-//            }
-//          } else {
-//            None
-//          }
-//        }
-//      }
-//
-//      private def workAroundTailRecIssue : Option[XmlEvent] = {
-//        Some(AddXmlElementEvent(fetchElementFromReader, getLocatorFromReader))
-//      }
-//
-//      @tailrec
-//      private def doEnumerate[V](nextEvent : Option[XmlEvent], i : Iteratee[XmlEvent,V]) : Iteratee[XmlEvent,V] = {
-//        i match {
-//          case i@Done(_,_) => i
-//          case Cont(k) =>
-//            if(nextEvent.isDefined) {
-//              if(outputBuffer.size < chunkSize) {
-//                val event = nextEvent.get
-//                outputBuffer += event
-//                doEnumerate[V](next, i)
-//              } else {
-//                val nextI = flushOutputBuffer(k)
-//                doEnumerate[V](next, nextI)
-//              }
-//            } else {
-//              flushOutputBuffer(k)
-//            }
-//        }
-//      }
-//
-//      private def flushOutputBuffer[V](k: (Input[XmlEvent]) => Iteratee[XmlEvent, V]) : Iteratee[XmlEvent, V] = {
-//        val buffer = outputBuffer.toList
-//        outputBuffer.clear
-//        k(El(buffer,Nil))
-//      }
-//
-//      private def getElementQNameFromReader : XsdQName = {
-//        val prefix = newXsdNCName(reader.getPrefix())
-//        val uri = newXsdAnyURI(reader.getNamespaceURI())
-//        val localName = new XsdNCName(reader.getLocalName())
-//        new XsdQName(prefix, uri, localName)
-//      }
-//
-//      private def fetchElementFromReader() : XmlElement = {
-//        val qName = getElementQNameFromReader
-//
-//        val attributes = for(i <- 0 until reader.getAttributeCount())
-//        yield {
-//          val prefix = newXsdNCName(reader.getPrefix())
-//          val uri = newXsdAnyURI(reader.getAttributeNamespace(i))
-//          val localName = new XsdNCName(reader.getAttributeLocalName(i))
-//          val qName = new XsdQName(prefix, uri, localName)
-//          val value = reader.getAttributeValue(i)
-//          qName -> value
-//        }
-//
-//        val prefixes =
-//          for(i <- 0 until reader.getNamespaceCount())
-//          yield {
-//            val prefix = newXsdNCName(reader.getNamespacePrefix(i))
-//            val uri = newXsdAnyURI(reader.getNamespaceURI(i))
-//            prefix -> uri
-//          }
-//
-//        // Note: this must come after all the other element parameters are extracted from reader since this call will change the reader's state
-////        val value = peekParseElementValue(next, Nil)
-//        val value = None
-//        XmlElement(qName, value, attributes.toMap, prefixes.toMap)
-//      }
-//
-//      private def newXsdAnyURI(uri: String) = {
-//        if(uri == null) {
-//          XmlConstants.NULL_NS_URI
-//        } else {
-//          new XsdAnyURI(uri)
-//        }
-//      }
-//      private def newXsdNCName(name : String) = {
-//        if(name == null) {
-//          XmlConstants.DEFAULT_NS_PREFIX
-//        } else {
-//          new XsdNCName(name)
-//        }
-//      }
-//      private def getLocatorFromReader : XmlLocator = {
-//        val l = reader.getLocation()
-//        XmlLocator(l.getCharacterOffset, l.getColumnNumber, l.getLineNumber, l.getPublicId, l.getSystemId)
-//      }
-//
-//      // Peek at the next few XmlEvents - if it is a string of text events followed by an end event then compress
-//      // the sequence by extracting the combined "value" of the text events and throw away the individual
-//      // text events
-//      @tailrec
-//      private def peekParseElementValue(nextEvent : Option[XmlEvent], textEvents : List[AddXmlTextEvent]) : Option[String] = {
-//        if(nextEvent.isDefined) {
-//          nextEvent.get match {
-//            case e:AddXmlTextEvent => {
-//              peekParseElementValue(next, e :: textEvents)
-//            }
-//            case e:EndXmlElementEvent => {
-//              inputQueue.enqueue(e)
-//              val result = textEvents.foldLeft(new StringBuilder) { (s,textEvent) => s.append(textEvent.text) }
-//              Some(result.toString)
-//            }
-//            case e:XmlEvent  => {
-//              textEvents.reverse foreach { inputQueue.enqueue(_) }
-//              inputQueue.enqueue(e)
-//              None
-//            }
-//          }
-//        } else {
-//          textEvents.reverse foreach { inputQueue.enqueue(_) }
-//          None
-//        }
-//      }
-//    }
-//  }
-//}
-//
